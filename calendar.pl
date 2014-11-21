49b68773cb018008dea089a51c8ff6f102032a6b
kmb3398 <kmb3398@rit.edu>
2014-11-20 23:55:32 -0500

    Fixed unescaped $ in awk commands.

--- calendar.pl
+++ calendar.pl
@@ -166,7 +166,7 @@ while (1) {
 			while (1) {
 				print "Please enter the event start time: ";
 				$st = <STDIN>;
-				$st = `echo $st | awk 'tolower($0) ~ /^(0?[[:digit:]]|1[0-2]):[0-5][[:digit:]](am|pm)$/ {print tolower($0)}'`
+				$st = `echo $st | awk 'tolower(\$0) ~ /^(0?[[:digit:]]|1[0-2]):[0-5][[:digit:]](am|pm)\$/ {print tolower(\$0)}'`;
 				if ($st ne "") {
 					last;
 				}
@@ -175,7 +175,7 @@ while (1) {
 			while (1) {
 				print "Please enter the event end time: ";
 				$et = <STDIN>;
-				$et = `echo $et | awk 'tolower($0) ~ /^(0?[[:digit:]]|1[0-2]):[0-5][[:digit:]](am|pm)$/ {print tolower($0)}'`
+				$et = `echo $et | awk 'tolower(\$0) ~ /^(0?[[:digit:]]|1[0-2]):[0-5][[:digit:]](am|pm)\$/ {print tolower(\$0)}'`;
 				if ($et ne "") {
 					last;
 				}
